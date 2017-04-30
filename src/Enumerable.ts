51d25a14ab7acaf8f79ad6f8e6b32304dd4f4fe1
Robert Rudman <rjrudman@gmail.com>
2017-04-30 17:41:39 +1000

    Make iterators easier to create

--- src/Enumerable.ts
+++ src/Enumerable.ts
@@ -9,12 +9,21 @@ export interface ResetableIterator<T> extends Iterator<T> {
 
 export class Enumerable<T> implements Iterable<T> {
     private iterator: ResetableIterator<T>;
-
     public static Of<T>(source: T[] | ResetableIterator<T>): Enumerable<T> {
         if (isIterator(source)) {
             return new Enumerable<T>(source);
         }
         return new Enumerable<T>(new EmptyIterator<T>(source));
+    }
+
+    private makeIterator<TReturnIteratorType>(sourceIterator: ResetableIterator<T>, thing: ((value?: any) => IteratorResult<TReturnIteratorType>)) {
+        const iterator: ResetableIterator<TReturnIteratorType> = {
+            next: thing,
+            reset: function () {
+                sourceIterator.reset();
+            }
+        }
+        return iterator;
     }
 
     protected constructor(iterator: ResetableIterator<T>) {
@@ -27,7 +36,25 @@ export class Enumerable<T> implements Iterable<T> {
     }
 
     public Where(predicate: (item: T) => boolean) {
-        return new Enumerable<T>(new WhereIterator<T>(this.iterator, predicate));
+        const sourceIterator = this.iterator;
+        const newIterator = this.makeIterator<T>(sourceIterator, function () {
+            let nextItem = sourceIterator.next();
+            if (nextItem.done) {
+                return nextItem;
+            }
+            while (!nextItem.done) {
+                if (!predicate || predicate(nextItem.value)) {
+                    return nextItem;
+                }
+                nextItem = sourceIterator.next();
+            }
+
+            return {
+                done: true,
+                value: <any>null
+            }
+        });
+        return Enumerable.Of(newIterator);
     }
 
     public First(predicate: ((item: T) => boolean) | undefined = undefined): T {
@@ -55,17 +82,39 @@ export class Enumerable<T> implements Iterable<T> {
     }
 
     public Select<TReturnType>(selector: (item: T) => TReturnType): Enumerable<TReturnType> {
-        return new Enumerable<TReturnType>(new SelectIterator<T, TReturnType>(this.iterator, selector));
+        const sourceIterator = this.iterator;
+        const newIterator = this.makeIterator<TReturnType>(sourceIterator, function () {
+            const nextItem = sourceIterator.next();
+            if (nextItem.done) {
+                return {
+                    done: true,
+                    value: <any>null
+                }
+            }
+            return {
+                done: false,
+                value: selector(nextItem.value)
+            };
+        });
+        return Enumerable.Of<TReturnType>(newIterator);
     }
 
     public Skip(num: number): Enumerable<T> {
-        for (let i = 0; i < num; i++) {
-            const currentItem = this.iterator.next();
-            if (currentItem.done) {
-                break;
+        let skipped = false;
+        const sourceIterator = this.iterator;
+        const newIterator = this.makeIterator<T>(sourceIterator, function () {
+            if (!skipped) {
+                let i = 0;
+                while (i < num) {
+                    sourceIterator.next();
+                    i++;
+                }
+                skipped = true;
             }
-        }
-        return Enumerable.Of(this.iterator);
+            return <IteratorResult<T>>sourceIterator.next()
+        });
+
+        return Enumerable.Of(newIterator);
     }
 
     public ToArray(): T[] {
@@ -101,65 +150,5 @@ class EmptyIterator<T> implements ResetableIterator<T> {
 
     reset(): void {
         this.pointer = 0;
-    }
-}
-
-class WhereIterator<T> implements ResetableIterator<T> {
-    private sourceIterator: ResetableIterator<T>;
-    private predicate: ((item: T) => boolean) | undefined;
-
-    public constructor(sourceIterator: ResetableIterator<T>, predicate: ((item: T) => boolean) | undefined) {
-        this.sourceIterator = sourceIterator;
-        this.predicate = predicate;
-    }
-
-    public next(): IteratorResult<T> {
-        let nextItem = this.sourceIterator.next();
-        if (nextItem.done) {
-            return nextItem;
-        }
-        while (!nextItem.done) {
-            if (!this.predicate || this.predicate(nextItem.value)) {
-                return nextItem;
-            }
-            nextItem = this.sourceIterator.next();
-        }
-
-        return {
-            done: true,
-            value: <any>null
-        }
-    }
-
-    public reset(): void {
-        this.sourceIterator.reset();
-    }
-}
-
-class SelectIterator<TSourceType, TReturnType> implements ResetableIterator<TReturnType> {
-    private sourceIterator: ResetableIterator<TSourceType>;
-    private selector: (item: TSourceType) => TReturnType;
-
-    public constructor(sourceIterator: ResetableIterator<TSourceType>, selector: (item: TSourceType) => TReturnType) {
-        this.sourceIterator = sourceIterator;
-        this.selector = selector;
-    }
-
-    public next(): IteratorResult<TReturnType> {
-        const nextItem = this.sourceIterator.next();
-        if (nextItem.done) {
-            return {
-                done: true,
-                value: <any>null
-            }
-        }
-        return {
-            done: false,
-            value: this.selector(nextItem.value)
-        };
-    }
-
-    public reset(): void {
-        this.sourceIterator.reset();
     }
 }
