fd7e7dc09d5c6b894473d96033cd5ea87dbbbb77
Robert Rudman <rjrudman@gmail.com>
2017-04-30 15:20:27 +1000

    Initial commit

---
+++ spec/basic.spec.ts
@@ -0,0 +1,8 @@
+describe("Greeter", () => {
+    describe("greet", () => {
+        it("returns Hello World", () => {
+            let result: string = "Hello World";
+            expect(result).toEqual("Hello World");
+        });
+    });
+});
